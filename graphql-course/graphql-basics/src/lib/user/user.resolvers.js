05b6c93d75aa137b0970b5264bad265ecc23105c
wkwksama <mailpakgasing@gmail.com>
2019-09-15 02:32:28 +0700

    boilerplate: resolver modular

---
+++ graphql-course/graphql-basics/src/lib/user/user.resolvers.js
@@ -0,0 +1,64 @@
+const users = [{
+  id: '1',
+  name: 'Andrew',
+  email: 'andrew@example.com',
+  age: 27
+}, {
+  id: '2',
+  name: 'Sarah',
+  email: 'sarah@example.com'
+}, {
+  id: '3',
+  name: 'Mike',
+  email: 'mike@example.com'
+}]
+
+const posts = [{
+  id: '10',
+  title: 'GraphQL 101',
+  body: 'This is how to use GraphQL...',
+  published: true,
+  author: '1'
+}, {
+  id: '11',
+  title: 'GraphQL 201',
+  body: 'This is an advanced GraphQL post...',
+  published: false,
+  author: '1'
+}, {
+  id: '12',
+  title: 'Programming Music',
+  body: '',
+  published: false,
+  author: '2'
+}]
+
+
+export default {
+  Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
+    },
+    me() {
+      return {
+        id: '123098',
+        name: 'Mike',
+        email: 'mike@example.com'
+      }
+    },
+  },
+
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id
+      })
+    }
+  }
+}
--- graphql-course/graphql-basics/src/typeDefs.js
+++ graphql-course/graphql-basics/src/typeDefs.js
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
 
-const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
+const typesArray = fileLoader(path.join(__dirname, './lib/**/*.graphql'));
 const typeDefs = mergeTypes(typesArray);
 
 export default typeDefs;
