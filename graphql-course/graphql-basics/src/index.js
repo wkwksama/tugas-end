56bc8cce80e293e1c6e26aeb3f2c67fda879d70c
wkwksama <mailpakgasing@gmail.com>
2019-09-14 20:50:35 +0700

    hello

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -1,1 +1,24 @@
-console.log('hello gql')
+import {GraphQLServer} from 'graphql-yoga'
+
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'Halo ini graphql yoga !'
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log('Server is running...')
+})
