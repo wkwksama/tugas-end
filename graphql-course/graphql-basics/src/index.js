84513a37f2295d013552ca289a4acff2e98f8865
wkwksama <mailpakgasing@gmail.com>
2019-09-15 00:32:01 +0700

    relation

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -22,17 +22,20 @@ const posts = [{
   id: '10',
   title: 'GraphQL 101',
   body: 'This is how to use GraphQL...',
-  published: true
+  published: true,
+  author: '1'
 }, {
   id: '11',
   title: 'GraphQL 201',
   body: 'This is an advanced GraphQL post...',
-  published: false
+  published: false,
+  author: '1'
 }, {
   id: '12',
   title: 'Programming Music',
   body: '',
-  published: false
+  published: false,
+  author: '2'
 }]
 
 // Type definitions (schema)
@@ -49,6 +52,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -56,6 +60,7 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
     }
 `
 
@@ -96,6 +101,20 @@ const resolvers = {
         body: '',
         published: false
       }
+    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id
+      })
     }
   }
 }
