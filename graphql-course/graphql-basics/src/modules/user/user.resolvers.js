6970f7e5fb8092b53c1dbed61eae370fce09b1b4
wkwksama <mailpakgasing@gmail.com>
2019-09-15 19:08:55 +0700

    mutation user

--- graphql-course/graphql-basics/src/modules/user/user.resolvers.js
+++ graphql-course/graphql-basics/src/modules/user/user.resolvers.js
@@ -1,3 +1,5 @@
+import uuidv4 from 'uuid/v4';
+
 const users = [{
   id: '1',
   name: 'Andrew',
@@ -13,7 +15,7 @@ const users = [{
   email: 'mike@example.com',
 }];
 
-const posts = [{
+let posts = [{
   id: '10',
   title: 'GraphQL 101',
   body: 'This is how to use GraphQL...',
@@ -49,6 +51,41 @@ export default {
         name: 'Mike',
         email: 'mike@example.com',
       };
+    },
+  },
+
+  Mutation: {
+    createUser(parent, args) {
+      const emailTaken = users.some((user) => user.email === args.data.email);
+
+      if (emailTaken) {
+        throw new Error('Email taken');
+      }
+
+      const user = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      users.push(user);
+
+      return user;
+    },
+    deleteUser(parent, args) {
+      const userIndex = users.findIndex((user) => user.id === args.id);
+
+      if (userIndex === -1) {
+        throw new Error('User not found');
+      }
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      posts = posts.filter((post) => {
+        const match = post.author === args.id;
+        return !match;
+      });
+
+      return deletedUsers[0];
     },
   },
 
