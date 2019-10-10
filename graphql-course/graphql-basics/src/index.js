d929959aac5c4c910a98089a99cd7bb2919b9e41
wkwksama <mailpakgasing@gmail.com>
2019-10-10 11:14:44 +0700

    setup env and mongo

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -1,12 +1,24 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { resolvers, typeDefs } from './schema';
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoUrl = process.env.MONGO_STRING || 'mongodb://localhost:27017/rakyat62';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoUrl, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('mongo connected', mongoUrl);
+  }
+});
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 });
 
-server.start((info) => {
+server.start({ port: process.env.PORT || 4000 }, (info) => {
   console.log('The server is up!', info);
 });
